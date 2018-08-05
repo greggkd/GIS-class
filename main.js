0924a0d95613aae0f78bb570bfc7e21d6ce32b27
Karen Gregg <karengregg@karens-mbp.attlocal.net>
2018-08-05 18:05:13 -0500

    commit with url that works VectorTile layer

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import MVT from 'ol/format/MVT';
 import VectorTileLayer from 'ol/layer/VectorTile';
 import VectorTileSource from 'ol/source/VectorTile';
 
-const key = 'arAYMSA6rBn1mQwOvOMZ';
+const key = '<arAYMSA6rBn1mQwOvOMZ>';
 
 const map = new Map({
     target: 'map-container',
@@ -22,8 +22,12 @@ const layer = new VectorTileLayer({
         '<a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>'
         ],
         format: new MVT(),
-        url: 'htts://free-{1-3}.tilehosting.com/data/v3/{z}/{x}/{y}.pbf.pict?key=${key}',
-        maxZoom: 14
+        //free.tilehosting.com -- works but can't get the file.
+        //url: 'https://free-{a-d}.tilehosting.com/data/v3/{z}/{x}/{y}.pbf.pict?key=arAYMSA6rBn1mQwOvOMZ',
+        //url: 'https://{a-d}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/' +
+        //'{z}/{x}/{y}.vector.pbf?access_token=' + key,
+        //maxZoom: 14
+        url: 'https://basemaps.arcgis.com/v1/arcgis/rest/services/World_Basemap/VectorTileServer/tile/{z}/{y}/{x}.pbf'
     })
 });
 map.addLayer(layer);
